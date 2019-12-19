3eb07219ccb2538e67f1a7c0bf5c680ad643fee0
madelei1 <2364119853@qq.com>
2019-12-19 20:03:11 +0800

    leetcode - 876--fanhuizhongjianjiedian

---
+++ oj_1/Solution_876.java
@@ -0,0 +1,46 @@
+package oj;
+/*给定一个带有头结点 head 的非空单链表，返回链表的中间结点。
+
+如果有两个中间结点，则返回第二个中间结点。
+示例 1：
+
+输入：[1,2,3,4,5]
+输出：此列表中的结点 3 (序列化形式：[3,4,5])
+返回的结点值为 3 。 (测评系统对该结点序列化表述是 [3,4,5])。
+注意，我们返回了一个 ListNode 类型的对象 ans，这样：
+ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, 以及 ans.next.next.next = NULL.
+示例 2：
+
+输入：[1,2,3,4,5,6]
+输出：此列表中的结点 4 (序列化形式：[4,5,6])
+由于该列表有两个中间结点，值分别为 3 和 4，我们返回第二个结点。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/middle-of-the-linked-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+
+
+public class Solution_876 {
+    public ListNode middleNode(ListNode head) {
+        /*//遍历二次的方法；
+        ListNode h = head;
+        int count = 0;
+        ListNode x = head;
+        while ( h != null) {
+            h = h.next;
+            count ++;
+        }
+        for (int i = 1; i <count/2 + 1; i++) {
+            x = x.next;
+        }
+        return  x;*/
+        //遍历一次求出
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
