86545791646fef4d365275987a43cc16e11670e3
madelei1 <2364119853@qq.com>
2019-12-23 20:22:24 +0800

    huanxinglianbiao

---
+++ DataStructure/Node.java
@@ -0,0 +1,10 @@
+package DataStructure;
+
+public class Node {
+    int val;
+    Node next;
+    public Node(int val, Node next) {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ DataStructure/Test_CircularLinkedList.java
@@ -0,0 +1,10 @@
+package DataStructure;
+
+public class Test_CircularLinkedList {
+    public static void main(String[] args) {
+        CircularLinkedList x = new CircularLinkedList();
+        x.addNode(5);
+        x.countNode(1,2,5);
+       // x.print();
+    }
+}
