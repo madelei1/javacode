5043848d4b32397f075ab30f546cb28e7d788478
madelei1 <2364119853@qq.com>
2020-01-01 17:31:13 +0800

    lianxi

---
+++ Practice1/List_2.java
@@ -0,0 +1,120 @@
+package oj;
+
+public class List_2 {
+    ListNode_2 head;
+    ListNode_2 last;
+    int size = 0;
+
+    //在链表的头部插入
+    public void addHead(int val) {
+        ListNode_2 node_1 = new ListNode_2(val,null);
+        if (head == null) {
+            head = node_1;
+            last = node_1;
+            size ++;
+            return;
+        } else {
+            node_1.next = head;
+            head = node_1;
+            size ++;
+        }
+    }
+    //在尾部插入
+    public void addLast(int val) {
+        ListNode_2 node_1 = new ListNode_2(val,null);
+        if (last == null) {
+            last = node_1;
+            size ++;
+            return;
+        } else {
+            last.next = node_1;
+            last = node_1;
+            size ++;
+        }
+    }
+    //在任意位置插入节点
+    public void addIndex(int index, int val) {
+        judge(index);
+        ListNode_2 node_1 = new ListNode_2(val,null);
+        if (index == 0) {
+            addHead(val);
+        } else if (index == size) {
+            addLast(val);
+        } else {
+            ListNode_2 x = returnIndex(index-1);
+            node_1.next = x.next;
+            x.next = node_1;
+            size ++;
+        }
+    }
+    //删除一个指定的值对应的链表节点
+    public void removeVal(int val) {
+        ListNode_2 x = head;
+        while (x.val == val) {
+            x = x.next;
+            size --;
+            return;
+        }
+        while (x.next != null) {
+            if (x.next.val == val) {
+                x.next = x.next.next;
+                size --;
+                return;
+            } else {
+                x = x.next;
+            }
+        }
+    }
+    //删除所有节点值为val的节点
+    public void removeAllVal(int val) {
+        ListNode_2 x = head;
+        while (x != null && x.val == val) {
+            x = x.next;
+            size --;
+        }
+        if (x  == null) {
+            return;
+        }
+        while ( x.next != null) {
+            if (x.next.val == val) {
+                x.next = x.next.next;
+                size --;
+            } else {
+                x = x.next;
+            }
+
+        }
+    }
+    //返回给出索引的值
+    public int returnVal(int index) {
+        return returnIndex(index).val;
+    }
+    //遍历链表
+    public void print() {
+        ListNode_2 x = head;
+        while (x != null) {
+            System.out.print(x.val+" ");
+            x = x.next;
+        }
+    }
+    //返回指定位置的节点
+    public ListNode_2 returnIndex(int index) {
+        judge(index);
+        ListNode_2 x = head;
+        for (int i = 0; i < index; i++) {
+            x = x.next;
+        }
+        return x;
+    }
+    //判断index是否合法
+    public void judge(int index) {
+        if (index < 0 || index > size) {
+            System.out.println("索引非法！");
+            return;
+        }
+    }
+    //返回链表的长度
+    public int size() {
+        return size;
+    }
+}
